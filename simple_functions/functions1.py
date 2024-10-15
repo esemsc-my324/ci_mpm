fe5893f089f85e6e6666fb5a56ee0e3c74774b84
esemsc-16a18a54 <my324@ic.ac.uk>
2024-10-15 11:59:41 +0100

    add factorial function

--- simple_functions/functions1.py
+++ simple_functions/functions1.py
@@ -1,5 +1,12 @@
+from functools import cache
 
-__all__ = ['my_sum']
+
+"""
+When from module import * is used,
+only the names listed in __all__ will be imported.
+"""
+
+__all__ = ["my_sum", "factorial"]
 
 
 def my_sum(iterable):
@@ -7,3 +14,8 @@ def my_sum(iterable):
     for i in iterable:
         tot += i
     return tot
+
+
+@cache
+def factorial(n):
+    return n * factorial(n - 1) if n else 1
--- tests/test_simple_functions.py
+++ tests/test_simple_functions.py
@@ -1,16 +1,21 @@
 import pytest
 
-from simple_functions import my_sum
+from simple_functions import my_sum, factorial
 
 
 class TestSimpleFunctions(object):
-    '''Class to test our simple functions are working correctly'''
+    """Class to test our simple functions are working correctly"""
 
-    @pytest.mark.parametrize('iterable, expected', [
-        ([8, 7, 5], 20),
-        ((10, -2, 5, -10, 1), 4)
-    ])
+    @pytest.mark.parametrize(
+        "iterable, expected", [([8, 7, 5], 20), ((10, -2, 5, -10, 1), 4)]
+    )
     def test_my_add(self, iterable, expected):
-        '''Test our add function'''
+        """Test our add function"""
         isum = my_sum(iterable)
         assert isum == expected
+
+    @pytest.mark.parametrize("n, expected", [(5, 120), (3, 6), (1, 1)])
+    def test_factorial(self, n, expected):
+        """Test our factorial function"""
+        result = factorial(n)
+        assert result == expected
